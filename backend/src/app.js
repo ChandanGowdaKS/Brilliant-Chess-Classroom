9d50ad367a8eef5bf9b0dcb6a65c3aa83601b18d
Chandan Gowda K S <chanduchess122@gmail.com>
2026-05-17 15:37:33 +0530

    initial commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ const io = connectToSocket(server);
 
 
 app.set("port", (process.env.PORT || 8000))
-app.use(cors());
+app.use(cors({ origin: process.env.CORS_ORIGIN || "http://localhost:3000" }));
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
