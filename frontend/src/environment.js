b4f509b4d268e3d0b174d7852b21b0d6e02d4929
Chandan Gowda K S <chanduchess122@gmail.com>
2026-05-17 15:41:18 +0530

    inital commit

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,10 @@
 let IS_PROD = process.env.NODE_ENV === 'production';
 const server = IS_PROD ?
     process.env.REACT_APP_BACKEND_URL || "https://brilliant-chess-classroom-2.onrender.com" :
-    process.env.REACT_APP_BACKEND_URL || "http://localhost:8000"
+    process.env.REACT_APP_BACKEND_URL || "http://localhost:8000";
+
+console.log('Backend Server URL:', server);
+console.log('Is Production:', IS_PROD);
 
 
 export default server;
